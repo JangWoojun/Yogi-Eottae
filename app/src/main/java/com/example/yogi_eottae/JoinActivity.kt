c168b1c5e3a3860585b65ed56d6d60b5bee395a6
JangWoojun <woojun0107@naver.com>
2022-09-10 03:01:29 +0900

    2022/09/10 미 회원가입 기능 구현

--- app/src/main/java/com/example/yogi_eottae/JoinActivity.kt
+++ app/src/main/java/com/example/yogi_eottae/JoinActivity.kt
@@ -1,11 +1,42 @@
 package com.example.yogi_eottae
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.emailArea)
+            val password = findViewById<EditText>(R.id.passwordArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 회원가입 성공시 main으로
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // 회원가입 실패시 Log 출력
+                        Log.w("JoinActivity", "createUserWithEmail:failure", task.exception)
+                    }
+                }
+        }
+
+
     }
 }
