00360fe73a1a47f2753e50c3acdec4e86df3cd5d
JangWoojun <woojun0107@naver.com>
2022-09-10 02:53:51 +0900

    2022/09/10 미 회원가입 시 회원가입 창으로 이동하기 구현

--- app/src/main/java/com/example/yogi_eottae/SplashActivity.kt
+++ app/src/main/java/com/example/yogi_eottae/SplashActivity.kt
@@ -4,16 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        auth = Firebase.auth
+
+        if (auth.currentUser?.uid == null){
+            Handler().postDelayed({
+                startActivity(Intent(this,JoinActivity::class.java))
+                finish()
+            },3000)
+        }
+        else {
+            Handler().postDelayed({
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            },3000)
+        }
+
         // 스플래쉬 화면을 메인 엑티비티에 넘겨준다 3초동안 보여주고
-        Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
-        },3000)
+
 
     }
 }
